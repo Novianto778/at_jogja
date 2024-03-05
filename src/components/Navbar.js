20265923f44e85adaa44fa6550a69320f92f310e
novianto778 <63406837+Novianto778@users.noreply.github.com>
2024-03-05 16:33:44 +0700

    fix: build error for useMediaQuery

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 "use client";
-import useDevice from "@/hooks/useDevice";
 import { cn } from "@/utils/lib";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+
+const useDevice = dynamic(() => import("@/hooks/useDevice"));
 
 const Navbar = ({ primary }) => {
   const [openMenu, setOpenMenu] = useState(false);
